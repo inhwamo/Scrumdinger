b582afe8a3ab819bcfbe2479af663a64099ca308
inhwamo <inhwa2000@gmail.com>
2023-03-31 02:40:55 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by InHwa Mo on 2023-03-31.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
